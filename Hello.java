ba4f5f1b381baf62611cbc4e7fd71a5df5147bfc
prasiddahal <giriraj.dahal16@gmail.com>
2021-07-17 00:50:43 +0545

    Prasid's Commit

--- Hello.java
+++ Hello.java
@@ -1,9 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Hello {
         public static void main(String[] args) {
 
                 System.out.println("Hello world");
                 System.out.println("Hello world");
                 System.out.println("Hello world");
+                System.out.println("My name is Khan and i am not a terrorist");
+
 
 
 
